ee44029fb1fe574fe2d43f7a3aaf311aded97bf7
tdeheurles <tdeheurles@gmail.com>
2015-06-16 14:32:03 +0200

    Corrected container tag

--- build/main.sh
+++ build/main.sh
@@ -7,7 +7,7 @@ artifact_tag="$artifact_name:$servicemajor.$serviceminor.$BUILD_NUMBER"
 
 # Build
 docker build -t $artifact_name .
-docker tag $artifact_tag
+docker tag $artifact_name $artifact_tag
 
 # Push to Google Cloud Engine
 gcloud preview docker push $artifact_name
