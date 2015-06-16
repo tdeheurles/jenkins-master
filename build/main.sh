d8cdf90ba59d3571a0479f18ccdd02b10d58dce8
tdeheurles <tdeheurles@gmail.com>
2015-06-16 14:22:35 +0200

    added build

---
+++ build/main.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# import names
+. ./build/release.cfg
+artifact_name="gcr.io/$projectid/$servicename"
+artifact_tag="$artifact_name:$servicemajor.$serviceminor.$BUILD_NUMBER"
+
+# Build
+docker build -t $artifact_name .
+docker tag $artifact_tag
+
+# Push to Google Cloud Engine
+gcloud preview docker push $artifact_name
+gcloud preview docker push $artifact_tag
